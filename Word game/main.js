019a48cfcd2fbc593d4f6f899b9e7960039ef717
SMH.Najafi <97471910+Najafi80@users.noreply.github.com>
2022-07-22 17:43:06 +0430

    Add files via upload

---
+++ Word game/main.js
@@ -0,0 +1,158 @@
+let kalamat = [
+    'ABOUT','ABOVE','ACTOR','ACUTE','ADEPT','ADMIT','ADOPT','ADORE','ADULT','AFTER','AGILE','AGREE','AISLE','ALBUM','ALERT','ALIEN','ALIKE','ALIVE','ALLOW','ALONG','ALOUD','ALTER','AMBER','AMEND','AMPLE','AMPLY','AMUSE','ANGEL','ANGLE','ANKLE','APPLE','APPLY','APRON','ARROW','ASSET','AVERT','AVOID','BACON','BAKER','BASIC','BASIL','BASIN','BATHE','BEACH','BEARD','BEAST','BEATS','BEGIN','BEGUN','BEING','BELOW','BIRCH','BIRTH','BLACK','BLANK','BLINK','BLOWN','BLUSH','BOARD','BOAST','BONUS','BOOST','BOUND','BOWEL','BRAID','BRAIN','BRAKE','BRAND','BRAVE','BREAD','BREAK','BRIDE','BRIEF','BRING','BRISK','BROKE','BROOM','BROWN','BULKY','BUNCH','BUYER','CABIN','CABLE','CAMEL','CANDY','CATER','CHAIN','CHAIR','CHALK','CHARM','CHART','CHASM','CHEAP','CHIEF','CHINA','CHIPS','CHOKE','CHOPS','CIGAR','CLAIM','CLAMP','CLASP','CLASS','CLEAN','CLEAR','CLIMB','CLOAK','CLONE','CLOSE','CLOTH','CLOUD','CLOVE','CLOWN','COAST','CORAL','COUNT','CRAFT','CRANE','CRANK','CRAWL','CRISP','CROSS','CROWD','CROWN','CRUSH','CRUST','CURVE','DAILY','DAIRY','DANCE','DATUM','DECAY','DECOR','DELAY','DEVIL','DIARY','DINER','DIRTY','DISCO','DIVER','DOUBT','DOUGH','DRAFT','DRAIN','DRAWN','DREAM','DRESS','DRINK','DRIVE','EARLY','EARTH','EIGHT','ELBOW','EMAIL','EMPTY','ENJOY','EQUAL','EQUIP','ETHIC','EXACT','EXIST','EXTRA','FACET','FAIRY','FAITH','FALSE','FANCY','FAULT','FAVOR','FEAST','FETCH','FIBER','FIELD','FINAL','FIRST','FLAIR','FLAKE','FLASK','FLICK','FLOAT','FLOCK','FLOWN','FLUSH','FLUTE','FOCAL','FOCUS','FORCE','FORUM','FOUND','FRAME','FRANK','FRAUD','FRESH','FRONT','FROST','FROZE','FRUIT','FUNGI','FUNNY','GHOST','GIVEN','GLASS','GLAZE','GLOBE','GLORY','GLOVE','GRACE','GRADE','GRAIN','GRAND','GRAPE','GRAPH','GRASP','GRASS','GRAVY','GREAT','GREET','GRILL','GROUP','GROWN','GUARD','GUESS','GUEST','GUIDE','HABIT','HAIRY','HAPPY','HARDY','HASTE','HAUNT','HEART','HEAVY','HEFTY','HONEY','HORSE','HOTEL','HOUSE','HUMAN','HUMID','HUMOR','HUSKY','IDEAL','IMAGE','IMPLY','INCUR','INDEX','INFER','INFRA','INLET','INNER','INPUT','INSET','IVORY','JEANS','JOINT','JUICE','LABOR','LADEN','LAPSE','LARGE','LATEX','LAUGH','LAYER','LEAFY','LEANS','LEARN','LEMON','LIGHT','LINER','LIVER','LIVES','LODGE','LOFTY','LOGIC','LOVER','LOWER','LUCID','LUCKY','LUNCH','LYMPH','MACRO','MAGIC','MAIZE','MAJOR','MAKER','MANGO','MAPLE','MARCH','MATCH','MEDAL','MEDIA','MERCY','MERIT','METAL','MICRO','MIDST','MIGHT','MINOR','MINUS','MIXER','MODEL','MOIST','MONEY','MONTH','MORAL','MOUNT','MOUSE','MOUTH','MOVER','MOVIE','MULTI','MUSIC','NAIVE','NEWLY','NEXUS','NICHE','NIGHT','NINJA','NOBLE','NODAL','NOISE','NOISY','NORTH','NOTCH','NOTED','NOVEL','NURSE','OCEAN','OFTEN','OLIVE','ONSET','OPERA','OPTIC','ORBIT','ORGAN','OTHER','OUGHT','OUNCE','OUTER','OVERT','OWING','OWNED','OWNER','OXIDE','PACED','PAINT','PANIC','PANTS','PARTY','PASTE','PATCH','PATIO','PAUSE','PAVED','PAYER','PEACH','PEARL','PEDAL','PHASE','PHONE','PIANO','PILOT','PINCH','PIVOT','PIZZA','PLACE','PLAIN','PLANE','PLANK','PLANT','PLATE','PLEAD','PLUCK','POINT','POKER','POLAR','PORCH','POUCH','POUND','POWER','PRESS','PRICE','PRIDE','PRIME','PRINT','PRIZE','PRONE','PROUD','PROVE','PUNCH','PUPPY','PURGE','PURSE','QUAIL','QUEEN','QUERY','QUEST','QUICK','QUIET','QUILT','QUITE','QUOTA','QUOTE','RADIO','RAINY','RAISE','RANCH','RANGE','RAPID','RATIO','REACH','REACT','READY','REALM','REGAL','REIGN','RELAX','RELAY','RELIC','REMIT','REPAY','REPLY','RESIN','RIDGE','RIGHT','RINSE','RISKY','RIVAL','ROAST','ROCKY','ROMAN','ROUGH','ROUND','ROYAL','RUSTY','SADLY','SAINT','SALON','SALTY','SANDY','SATIN','SAUCE','SCALE','SCARE','SCARF','SCARY','SCENT','SCORE','SCOUT','SCREW','SERUM','SETUP','SHADE','SHADY','SHAKE','SHAKY','SHAME','SHAPE','SHARE','SHARK','SHARP','SHAVE','SHEAR','SHELF','SHELL','SHIFT','SHINE','SHIRT','SHOCK','SHORE','SHORT','SHOUT','SHOVE','SHOWN','SHRUB','SHRUG','SIGHT','SIGMA','SILKY','SINCE','SIXTY','SKATE','SKILL','SKIRT','SLACK','SLATE','SLEEP','SLEPT','SLICE','SLICK','SLIDE','SLING','SLUMP','SMALL','SMART','SMILE','SMOKE','SMOKY','SNACK','SNAIL','SNAKE','SNEAK','SOBER','SOLAR','SOLVE','SOUND','SOUTH','SPACE','SPADE','SPARE','SPARK','SPEAK','SPEAR','SPELL','SPICE','SPICY','SPIKE','SPILL','SPINE','SPLIT','SPOIL','SPOKE','SPORT','SPRAY','SQUAD','SQUAT','SQUID','STACK','STAFF','STAGE','STAIN','STAIR','STAKE','STALE','STAMP','STAND','STARE','STEAD','STEAK','STEAL','STEAM','STEEL','STERN','STICK','STILL','STING','STOCK','STONE','STORE','STORM','STORY','STOVE','STRAP','STRAW','STRAY','STUCK','STUDY','STUFF','STUMP','STYLE','SUGAR','SUITE','SUNNY','SUPER','SWAMP','SWEAT','SWEPT','SWIFT','SWINE','SWING','SWIRL','SYRUP','TABLE','TAKEN','TEACH','TEMPO','THANK','THEIR','THICK','THING','THINK','THIRD','THORN','THOSE','THREE','THROW','THUMB','THYME','TIDAL','TIGER','TIMER','TODAY','TOKEN','TONIC','TOUCH','TOUGH','TOWEL','TOWER','TOXIC','TOXIN','TRACE','TRACK','TRADE','TRAIL','TRAIN','TRASH','TREAD','TREND','TRIAD','TRIAL','TRIBE','TRICK','TWICE','TWINS','ULCER','ULTRA','UNCLE','UNDER','UNIFY','UNITE','UNITY','UPSET','URBAN','USAGE','VAGUE','VALID','VALUE','VIDEO','VIRAL','VITAL','VOCAL','VODKA','VOICE','VOWEL','WAFER','WAGED','WAGER','WAGON','WAIST','WAIVE','WASTE','WATCH','WATER','WEARY','WEIGH','WEIRD','WHALE','WHARF','WHEAT','WHILE','WHITE','WHOLE','WHOSE','WIDEN','WIDTH','WINDY','WOMAN','WOMEN','WORLD','WORSE','WORST','WORTH','WOULD','WOUND','WOVEN','WRECK','WRITE','WRONG','YEAST','YIELD','YOUNG','YOUTH'
+]; // number : 672 , index 0 ta 671
+
+//رمز بازی
+let kalame = Math.floor(Math.random()*670)+2 ;
+let ramz = kalamat[kalame] ;
+console.log(ramz)
+
+
+
+// تعیین سطح بازِی
+let tain_sath = document.querySelector('button#b2');
+let radio_button = document.querySelector('.radio_button');
+tain_sath.addEventListener('click', ()=>{
+    radio_button.classList.toggle('active')
+    
+}
+);
+
+
+
+//پیشنهاد ربات بر اساس سختی بازی    
+let r1 = document.querySelector('#r1')
+let r2 = document.querySelector('#r2')
+let r3 = document.querySelector('#r3')
+let a = document.querySelector('#inputs');
+let b;
+
+
+    let pishnahad_robot = setInterval(()=>{
+
+        
+        if( r1.checked ){
+
+            while(true){
+
+                b = kalamat[Math.floor(Math.random()*670)+2]
+
+                if (b.includes(ramz[0])||b.includes(ramz[1])||b.includes(ramz[2])||b.includes(ramz[3])||b.includes(ramz[4])){
+                    b=b
+                    break; 
+                }
+            } 
+
+
+        }else if( r2.checked ){
+
+            b = kalamat[Math.floor(Math.random()*670)+2]
+
+        }else if(r3.checked){
+
+            while(true){
+
+                b = kalamat[Math.floor(Math.random()*670)+2]
+                
+                if ( !(b.includes(ramz[0])||b.includes(ramz[1])||b.includes(ramz[2])||b.includes(ramz[3])||b.includes(ramz[4])) ) {
+                    b=b
+                    break; 
+                }
+            } 
+
+
+
+        }
+
+
+        let robot = document.createElement('p')
+
+        robot.innerHTML =`
+        <span id='s0'> ${b[0]} </span>
+        <span id='s1'> ${b[1]} </span>
+        <span id='s2'> ${b[2]} </span>
+        <span id='s3'> ${b[3]} </span>
+        <span id='s4'> ${b[4]} </span>
+        ` 
+        a.insertBefore(robot, a.firstElementChild);
+
+
+        for (let i=0 ; i<5 ; i++){ 
+
+            if (b === ramz) {
+                document.querySelector(`#s${i}`).style.color = 'green';
+                continue;
+            }else if (b[i] === ramz[i]){        
+                document.querySelector(`#s${i}`).style.color = 'green';
+            }else if(ramz.includes(b[i])){
+                document.querySelector(`#s${i}`).style.color = 'yellow';
+            }
+                
+        }
+
+    },3000)
+
+
+
+
+
+
+
+
+let inputs = Array.from(document.querySelectorAll('.form1>input')) ;
+let dokme = document.querySelector('button');
+
+let chap_kalame = dokme.addEventListener('click',()=>{
+        let hads_karbar= `${inputs[0].value}${inputs[1].value}${inputs[2].value}${inputs[3].value}${inputs[4].value}`.toUpperCase();
+    
+  
+
+        if (hads_karbar.length === 5 ){
+            let x = document.createElement('p');
+            x.innerHTML =`
+            <span id='s0'>${hads_karbar[0]} </span>
+            <span id='s1'>${hads_karbar[1]} </span>
+            <span id='s2'>${hads_karbar[2]} </span>
+            <span id='s3'>${hads_karbar[3]} </span>
+            <span id='s4'>${hads_karbar[4]} </span>
+            ` 
+            let y = document.querySelector('#inputs');
+            x.style['background-color']= 'rgb(161, 146, 255)';
+            y.insertBefore(x , y.firstElementChild);
+            
+
+
+            for (let i=0 ; i<5 ; i++){ 
+
+                if (hads_karbar === ramz) {
+                    document.querySelector(`#s${i}`).style.color = 'green';
+                    document.querySelector('.alert').classList.add('active2')
+                    document.querySelector('#b1').setAttribute('disabled','true')
+                    document.querySelector('#b2').setAttribute('disabled','true')
+                    clearInterval(pishnahad_robot)
+                    
+                }else if (hads_karbar[i] === ramz[i]){        
+                    document.querySelector(`#s${i}`).style.color = 'green';                   
+                }else if(ramz.includes(hads_karbar[i])){
+                    document.querySelector(`#s${i}`).style.color = 'yellow';
+                }
+                    
+            }
+    
+    
+        }else{
+            let hoshdar = document.querySelector('#hoshdar')
+            let p_hoshdar =document.createElement('p');
+            p_hoshdar.textContent = 'Fill in all the fields!'
+            p_hoshdar.style['color'] = 'red'
+            hoshdar.insertBefore(p_hoshdar,hoshdar.firstElementChild)
+            setTimeout(()=> hoshdar.removeChild(p_hoshdar),3000)
+        }
+});
+
+//دوباره بازی میکنم
+function reload(){
+    location.reload(); 
+   };
+
+
